6cb769e31e7eb8d41b0c4f2c72fbbac0dd0bb765
phatnguyen <phatnguyen@gmx.de>
2025-08-12 10:47:27 +0200

    feat: wip

--- app/pages/jobs/[id].vue
+++ app/pages/jobs/[id].vue
@@ -14,6 +14,8 @@ if (!job.value) {
     fatal: true,
   })
 }
+
+const favorited = shallowRef(false)
 </script>
 
 <template>
@@ -43,65 +45,83 @@ if (!job.value) {
 
     <template #body>
       <div>
-        <div class="flex gap-2 -ml-1 mb-2">
-          <UBadge
-            v-for="(typ, i) in job.jobtypen"
-            :key="typ"
-            :color="i === 0 ? 'primary' : 'neutral'"
-            :variant="i === 0 ? 'subtle' : 'subtle'"
+        <div class="flex justify-between">
+          <div>
+            <div class="flex gap-2 -ml-1 mb-2">
+              <UBadge
+                v-for="(typ, i) in job.jobtypen"
+                :key="typ"
+                :color="i === 0 ? 'primary' : 'neutral'"
+                :variant="i === 0 ? 'subtle' : 'subtle'"
+              >
+                {{ typ }}
+              </UBadge>
+            </div>
+
+            <h2 class="font-semibold text-2xl leading-tight text-balance mb-4">
+              {{ job.angebotstitel }}
+            </h2>
+
+            <div class="grid gap-1.5 text-sm mb-4">
+              <div class="flex items-center gap-2">
+                <UIcon name="i-lucide-building-2" /> {{ job.firma }}
+              </div>
+              <div class="flex items-center gap-2">
+                <UIcon name="i-lucide-map-pin" /> {{ job.arbeitsort }}
+              </div>
+
+              <div class="flex items-center gap-2 my-1">
+                <UBadge color="neutral" variant="outline">
+                  <UIcon name="i-lucide-clock" />
+                  <template v-if="job.arbeitszeitMin >= job.arbeitszeitMax">
+                    {{ job.arbeitszeitMax }}h
+                  </template>
+                  <template v-else>
+                    {{ job.arbeitszeitMin }}-{{ job.arbeitszeitMax }}h
+                  </template>
+                </UBadge>
+                <UBadge v-if="job.homeoffice" color="neutral" variant="outline">
+                  <UIcon name="i-lucide-house" /> {{ job.homeoffice[0] }}
+                </UBadge>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <UButton
+              icon="i-lucide-heart"
+              :variant="favorited ? 'subtle' : 'ghost'"
+              @click="favorited = !favorited"
+            >
+              {{ favorited ? 'Favorited' : 'Favorite' }}
+            </UButton>
+          </div>
+        </div>
+
+        <div class="text-sm">
+          <template
+            v-for="key in ['anzeigeText',
+                           'einleitungTitel',
+                           'einleitungText',
+                           'aufgabenTitel',
+                           'aufgabenText',
+                           'erwartungenTitel',
+                           'erwartungenText',
+                           'angebotTitel',
+                           'angebotText',
+                           'kontaktTitel',
+                           'kontaktText',
+            ] as (keyof typeof job)[]"
+            :key="key"
           >
-            {{ typ }}
-          </UBadge>
-        </div>
-
-        <h2 class="font-semibold text-2xl leading-tight text-balance mb-4">
-          {{ job.angebotstitel }}
-        </h2>
-
-        <div class="grid gap-1.5 text-sm mb-4">
-          <div class="flex items-center gap-2">
-            <UIcon name="i-lucide-building-2" /> {{ job.firma }}
-          </div>
-          <div class="flex items-center gap-2">
-            <UIcon name="i-lucide-map-pin" /> {{ job.arbeitsort }}
-          </div>
-
-          <div class="flex items-center gap-2 my-1">
-            <UBadge color="neutral" variant="outline">
-              <UIcon name="i-lucide-clock" />
-              <template v-if="job.arbeitszeitMin >= job.arbeitszeitMax">
-                {{ job.arbeitszeitMax }}h
-              </template>
-              <template v-else>
-                {{ job.arbeitszeitMin }}-{{ job.arbeitszeitMax }}h
-              </template>
-            </UBadge>
-            <UBadge v-if="job.homeoffice" color="neutral" variant="outline">
-              <UIcon name="i-lucide-house" /> {{ job.homeoffice[0] }}
-            </UBadge>
-          </div>
-        </div>
-
-        <div>
-          <div v-html="job.anzeigeText" />
-          <div v-html="job.einleitungTitel" />
-          <div v-html="job.einleitungText" />
-          <div v-html="job.aufgabenTitel" />
-          <div v-html="job.aufgabenText" />
-          <div v-html="job.erwartungenTitel" />
-          <div v-html="job.erwartungenText" />
-          <div v-html="job.angebotTitel" />
-          <div v-html="job.angebotText" />
-          <div v-html="job.kontaktTitel" />
-          <div v-html="job.kontaktText" />
+            <MDCCached
+              v-if="job[key]"
+              :value="job[key].toString()"
+              :cache-key="`${job.jobId}-${key}`"
+            />
+          </template>
         </div>
       </div>
-
-      <!-- <MDCCached
-        v-if="job.description"
-        :value="job.description"
-        :cache-key="job.id"
-      /> -->
     </template>
   </UDashboardPanel>
 </template>
